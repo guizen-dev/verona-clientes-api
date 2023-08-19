e4ffd2a10b487f43a244684776b0157c3f8d46c6
Joelson Campos <guilherme.campos3931@gmail.com>
2023-08-19 14:57:36 -0300

    cors error solve

--- veronaclientes/settings.py
+++ veronaclientes/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'clientes',
 ]
 
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
 
 ROOT_URLCONF = 'veronaclientes.urls'
